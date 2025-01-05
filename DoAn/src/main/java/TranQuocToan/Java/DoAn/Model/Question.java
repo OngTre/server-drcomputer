901a1f88d35d5cab37f2fca4553fce51893230a8
OngDevTre <96135272+OngTre@users.noreply.github.com>
2025-01-05 19:38:20 +0700

    Update Question.java

--- DoAn/src/main/java/TranQuocToan/Java/DoAn/Model/Question.java
+++ DoAn/src/main/java/TranQuocToan/Java/DoAn/Model/Question.java
@@ -18,71 +18,22 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Question cannot be blank")
+    @NotBlank(message = "")
     private String question;
 
     @NotBlank
-    private String subject = "Default Subject";
+    private String subject;
 
     @NotBlank
-    private String questionType = "single";
+    private String questionType;
 
-    // Định nghĩa bảng phụ cho các lựa chọn
     @ElementCollection
     @CollectionTable(name = "question_choices", joinColumns = @JoinColumn(name = "question_id"))
-    @Column(name = "choice")
     private List<String> choices;
 
-    // Định nghĩa bảng phụ cho các câu trả lời đúng
     @ElementCollection
     @CollectionTable(name = "question_correct_answers", joinColumns = @JoinColumn(name = "question_id"))
-    @Column(name = "correct_answer")
     private List<String> correctAnswers;
-
-    // Getter và Setter cho 'choices'
-    public List<String> getChoices() {
-        return choices;
-    }
-
-    public void setChoices(List<String> choices) {
-        this.choices = choices;
-    }
-
-    // Getter và Setter cho 'correctAnswers'
-    public List<String> getCorrectAnswers() {
-        return correctAnswers;
-    }
-
-    public void setCorrectAnswers(List<String> correctAnswers) {
-        this.correctAnswers = correctAnswers;
-    }
-
-    // Getter và Setter cho 'question'
-    public String getQuestionText() {
-        return question;
-    }
-
-    public void setQuestionText(String question) {
-        this.question = question;
-    }
-
-    // Getter và Setter cho 'subject'
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    // Getter và Setter cho 'questionType'
-    public String getQuestionType() {
-        return questionType;
-    }
-
-    public void setQuestionType(String questionType) {
-        this.questionType = questionType;
-    }
 }
 // package TranQuocToan.Java.DoAn.Model;
 
